447d9970a809d20b80c806f33eba977fbe164329
k2works <kakimomokuri@gmail.com>
2019-02-20 18:05:55 +0900

    refactor: Extract Variable

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -4,10 +4,13 @@ defmodule FizzBuzz do
   def generate_list(), do: 1..100 |> Enum.map &generate/1
 
   def generate(number) do
+    fizz = rem(number, 3) == 0
+    buzz = rem(number, 5) == 0
+
     cond do
-      rem(number, 3) == 0 and rem(number, 5) == 0 -> "FizzBuzz"
-      rem(number, 3) == 0 -> "Fizz"
-      rem(number, 5) == 0 -> "Buzz"
+      fizz and buzz -> "FizzBuzz"
+      fizz -> "Fizz"
+      buzz -> "Buzz"
       true -> number
     end
   end
