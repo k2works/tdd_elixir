5e61e9b1ea52f4e7ad3c0863b9c12700417536bd
k2works <kakimomokuri@gmail.com>
2019-02-20 18:12:56 +0900

    refactor: Substitute Algorithm

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -4,14 +4,14 @@ defmodule FizzBuzz do
   def generate_list(), do: 1..100 |> Enum.map &generate/1
 
   def generate(number) do
-    cond do
-      _fizz?(number) and _buzz?(number) -> "FizzBuzz"
-      _fizz?(number) -> "Fizz"
-      _buzz?(number) -> "Buzz"
-      true -> number
+    case {_fizz?(number), _buzz?(number)} do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      {_, _} -> number
     end
   end
 
-  defp _fizz?(number), do: rem(number, 3) == 0
-  defp _buzz?(number), do: rem(number, 5) == 0
+  defp _fizz?(number), do: rem(number, 3)
+  defp _buzz?(number), do: rem(number, 5)
 end
