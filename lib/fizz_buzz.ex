0877726c02e782733256e4f299177d31bbd6c4d3
k2works <kakimomokuri@gmail.com>
2019-02-20 17:52:05 +0900

    style: パイプ演算子

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,7 +1,7 @@
 defmodule FizzBuzz do
-  def print(), do: Enum.map generate_list(), &IO.puts/1
+  def print(), do: generate_list() |> Enum.map &IO.puts/1
 
-  def generate_list(), do: Enum.map 1..100, &generate/1
+  def generate_list(), do: 1..100 |> Enum.map &generate/1
 
   def generate(number) do
     cond do
