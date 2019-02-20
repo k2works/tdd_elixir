2dd808cd08581d0ba8701d7815a314543e50aeea
k2works <kakimomokuri@gmail.com>
2019-02-20 19:21:34 +0900

    test: 型チェック

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -4,15 +4,19 @@ defmodule FizzBuzz do
   @fizz "Fizz"
   @buzz "Buzz"
 
-  def print(), do: generate_list(@max_range) |> Enum.map &IO.puts/1
+  @type t :: binary()
+  @spec print() :: [t()]
+  def print(), do: generate_list(@max_range) |> Enum.map(&IO.puts/1)
 
-  def generate_list(max_range), do: 1..max_range |> Enum.map &generate/1
+  @spec generate_list(pos_integer()) :: [t()]
+  def generate_list(max_range), do: 1..max_range |> Enum.map(&generate/1)
 
+  @spec generate(pos_integer()) :: t()
   def generate(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: @fizz_buzz
   def generate(number) when rem(number, 3) == 0 , do: @fizz
   def generate(number) when rem(number, 5) == 0 , do: @buzz
   def generate(number) do
     unless is_number(number), do: raise RuntimeError, message: "数字以外は処理できません"
-    number
+    Integer.to_string(number)
   end
 end
--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -9,7 +9,7 @@ defmodule FizzBuzzTest do
 
   test "1から100までの数をプリントする,ただし3で割り切れる場合はFizz,5で割り切れる場合はBuzz,両方で割り切れる場合はFizzBuzzをプリントする" do
     list = FizzBuzz.generate_list(100)
-    assert List.first(list) == 1
+    assert List.first(list) == "1"
     assert Enum.at(list, 2) == "Fizz"
     assert Enum.at(list, 4) == "Buzz"
     assert Enum.at(list, 14) == "FizzBuzz"
@@ -19,7 +19,7 @@ defmodule FizzBuzzTest do
 
   test "3で割り切れる場合はFizzを返す" do
     assert FizzBuzz.generate(3) == "Fizz"
-    assert FizzBuzz.generate(1) == 1
+    assert FizzBuzz.generate(1) == "1"
   end
 
   test "5で割り切れる場合はBuzzを返す" do
