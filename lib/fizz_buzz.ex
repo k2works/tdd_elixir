66ad31ea9a751f5bb9b439b60cfc9d661ebbeaf5
k2works <kakimomokuri@gmail.com>
2019-02-20 17:27:13 +0900

    refactor: Hide Delegate

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,10 +1,11 @@
 defmodule FizzBuzz do
-  def print, do: downto(100, [])
-  def downto(0, result), do: result
-  def downto(current, result) do
+  def print, do: _downto(100, [])
+
+  defp _downto(0, result), do: result
+  defp _downto(current, result) do
     value = generate(current)
     IO.puts(value)
-    downto(current - 1, [ value | result] )
+    _downto(current - 1, [ value | result] )
   end
 
   def generate(number) do
