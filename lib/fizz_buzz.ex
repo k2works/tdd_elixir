1af62b72e56644b6bb464d0d49236f0e3a28e1d8
k2works <kakimomokuri@gmail.com>
2019-02-20 18:19:34 +0900

    refactor: Extract Function

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -3,15 +3,8 @@ defmodule FizzBuzz do
 
   def generate_list(), do: 1..100 |> Enum.map &generate/1
 
-  def generate(number) do
-    case {_fizz?(number), _buzz?(number)} do
-      {0, 0} -> "FizzBuzz"
-      {0, _} -> "Fizz"
-      {_, 0} -> "Buzz"
-      {_, _} -> number
-    end
-  end
-
-  defp _fizz?(number), do: rem(number, 3)
-  defp _buzz?(number), do: rem(number, 5)
+  def generate(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: "FizzBuzz"
+  def generate(number) when rem(number, 3) == 0 , do: "Fizz"
+  def generate(number) when rem(number, 5) == 0 , do: "Buzz"
+  def generate(number), do: number
 end
