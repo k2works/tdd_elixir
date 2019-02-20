bfca978711e5ef43ac911c6bc5f8d1cc2b3513e7
k2works <kakimomokuri@gmail.com>
2019-02-20 16:48:20 +0900

    test: 1から100までの数をプリントする
    学習テスト

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,5 +1,14 @@
 defmodule FizzBuzz do
   def print do
-    IO.puts(1)
+    print(1, 100, [])
+  end
+
+  def print(_current, 0, result) do
+    Enum.reverse(result)
+  end
+
+  def print(current, left, result) do
+    IO.puts(current)
+    print(current + 1, left - 1, [ current | result] )
   end
 end
