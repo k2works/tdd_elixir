44ad7118e9f881eeb68912e512fe18aa55289c13
k2works <kakimomokuri@gmail.com>
2019-02-23 12:23:10 +0900

    refactor: Extract Parameter

--- lib/tdd_elixir/fizz_buzz.ex
+++ lib/tdd_elixir/fizz_buzz.ex
@@ -1,7 +1,7 @@
 defmodule FizzBuzz do
   @moduledoc false
-  def print, do: generate_list() |> Enum.map(&IO.puts/1)
-  def generate_list(), do: 1..100 |> Enum.map(&generate/1)
+  def print, do: generate_list(100) |> Enum.map(&IO.puts/1)
+  def generate_list(max_range), do: 1..max_range |> Enum.map(&generate/1)
 
   def generate(number) do
     cond do
