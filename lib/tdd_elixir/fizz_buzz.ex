68749c3c07f5be27777ab798ec09ad5d5c7d7e5b
k2works <kakimomokuri@gmail.com>
2019-02-23 12:19:29 +0900

    refactor: Inline Variable
    &記法

--- lib/tdd_elixir/fizz_buzz.ex
+++ lib/tdd_elixir/fizz_buzz.ex
@@ -1,7 +1,7 @@
 defmodule FizzBuzz do
   @moduledoc false
-  def print, do: Enum.map(generate_list(), fn value -> IO.puts(value) end)
-  def generate_list(), do: Enum.map(1..100, fn number -> generate(number) end)
+  def print, do: Enum.map(generate_list(), &IO.puts/1)
+  def generate_list(), do: Enum.map(1..100, &generate/1)
 
   def generate(number) do
     cond do
