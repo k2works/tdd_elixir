3379c58c4b4fe5b8bcfcd0d6990bc7698fe14c00
k2works <kakimomokuri@gmail.com>
2019-02-23 12:25:28 +0900

    refactor: Replace Magic Number with Symbolic Constant

--- lib/tdd_elixir/fizz_buzz.ex
+++ lib/tdd_elixir/fizz_buzz.ex
@@ -1,6 +1,7 @@
 defmodule FizzBuzz do
+  @max_range 100
   @moduledoc false
-  def print, do: generate_list(100) |> Enum.map(&IO.puts/1)
+  def print, do: generate_list(@max_range) |> Enum.map(&IO.puts/1)
   def generate_list(max_range), do: 1..max_range |> Enum.map(&generate/1)
 
   def generate(number) do
