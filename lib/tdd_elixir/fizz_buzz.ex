1db7491fbb80343335358f54dee773faf969f0bf
k2works <kakimomokuri@gmail.com>
2019-02-23 12:40:06 +0900

    refactor: Replace Magic Number With Symbolic Constant

--- lib/tdd_elixir/fizz_buzz.ex
+++ lib/tdd_elixir/fizz_buzz.ex
@@ -4,8 +4,11 @@ defmodule FizzBuzz do
   def print, do: generate_list(@max_range) |> Enum.map(&IO.puts/1)
   def generate_list(max_range), do: 1..max_range |> Enum.map(&generate/1)
 
-  def generate(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: "FizzBuzz"
-  def generate(number) when rem(number, 3) == 0, do: "Fizz"
-  def generate(number) when rem(number, 5) == 0, do: "Buzz"
+  @fizz_buzz "FizzBuzz"
+  @fizz "Fizz"
+  @buzz "Buzz"
+  def generate(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: @fizz_buzz
+  def generate(number) when rem(number, 3) == 0, do: @fizz
+  def generate(number) when rem(number, 5) == 0, do: @buzz
   def generate(number), do: number
 end
