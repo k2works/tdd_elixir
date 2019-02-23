667cb36923b9778edb84267c9f6f5091c13ed336
k2works <kakimomokuri@gmail.com>
2019-02-23 14:21:17 +0900

    chore: 開発パッケージセットアップ

--- mix.exs
+++ mix.exs
@@ -23,11 +23,11 @@ defmodule TddElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:dialyze, "~> 0.1.3"},
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19"},
-      {:earmark, "~> 1.3"}
+      {:earmark, "~> 1.3"},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
