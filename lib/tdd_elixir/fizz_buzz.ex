b2c098cd3a93371b8814a799f3305b37121624d2
k2works <kakimomokuri@gmail.com>
2019-02-23 11:57:28 +0900

    refactor: do:形式

--- lib/tdd_elixir/fizz_buzz.ex
+++ lib/tdd_elixir/fizz_buzz.ex
@@ -1,12 +1,7 @@
 defmodule FizzBuzz do
   @moduledoc false
-  def print do
-    print(100, [])
-  end
-
-  def print(0, result) do
-    result
-  end
+  def print, do: print(100, [])
+  def print(0, result), do: result
 
   def print(current, result) do
     value = generate(current)
