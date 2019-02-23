3c6157c9976557ecc78b483dd2329ad0734e1591
k2works <kakimomokuri@gmail.com>
2019-02-23 12:38:06 +0900

    refactor: Replace Nested Conditional with Guard Clauses

--- lib/tdd_elixir/fizz_buzz.ex
+++ lib/tdd_elixir/fizz_buzz.ex
@@ -4,15 +4,8 @@ defmodule FizzBuzz do
   def print, do: generate_list(@max_range) |> Enum.map(&IO.puts/1)
   def generate_list(max_range), do: 1..max_range |> Enum.map(&generate/1)
 
-  def generate(number) do
-    case {_fizz?(number), _buzz?(number)} do
-      {0, 0} -> "FizzBuzz"
-      {0, _} -> "Fizz"
-      {_, 0} -> "Buzz"
-      {_, _} -> number
-    end
-  end
-
-  defp _fizz?(number), do: rem(number, 3)
-  defp _buzz?(number), do: rem(number, 5)
+  def generate(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: "FizzBuzz"
+  def generate(number) when rem(number, 3) == 0, do: "Fizz"
+  def generate(number) when rem(number, 5) == 0, do: "Buzz"
+  def generate(number), do: number
 end
